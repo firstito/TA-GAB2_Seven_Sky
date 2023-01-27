d550b2daff32c340abb0bfa03f16cb176139b8d0
yoga0123456789 <yogapratama@teknokrat.ac.id>
2023-01-27 18:39:49 +0700

    YogaPratama: melakukan perubahan pada form config.php pada bagian url akses websitenya

--- webservice-client/application/config/config.php
+++ webservice-client/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/rest-api/webservice-client';
+$config['base_url'] = 'http://localhost/TA-GAB2-SevenSky/webservice-client/';
 
 /*
 |--------------------------------------------------------------------------
